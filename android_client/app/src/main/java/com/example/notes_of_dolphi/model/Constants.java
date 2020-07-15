851283575e8a2bf6d2285816615a3e83a997a763
Bananiys202024 <BestJavaDeveloper24@gmail.com>
2020-07-15 15:17:20 +0300

    refactor

--- android_client/app/src/main/java/com/example/notes_of_dolphi/model/Constants.java
+++ android_client/app/src/main/java/com/example/notes_of_dolphi/model/Constants.java
@@ -2,7 +2,7 @@ package com.example.notes_of_dolphi.model;
 
 public class Constants {
 
-	final static int PORT = 2870;
+	final static int PORT = 2880;
 	final static String ip_host = "192.168.43.244";
 	final static String DATABASE_NAME = "notebook";
 	//ip 60:14:B3:5C:5C:13
