57ec5020c57f63a8b2c508c4b3e1918210ac0dcc
Bananiys202024 <BestJavaDeveloper24@gmail.com>
2020-07-21 15:07:30 +0100

    refactor

--- server/src/main/java/com/example/notes_of_dolphi/model/Constants.java
+++ server/src/main/java/com/example/notes_of_dolphi/model/Constants.java
@@ -2,8 +2,8 @@
 
 public class Constants {
 
-	final static int PORT = 2879;
-	final static String ip_host = "localhost";
+	final static int PORT = 2880;
+	final static String ip_host = "raspberrypi";
 	
 	public static int getPort() {
 		return PORT;
